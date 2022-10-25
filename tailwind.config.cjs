44f7f03843a219c6d2ac97c2ef00987f9cee6bcb
Sahan Punchihewa <sahannilupul18@gmail.com>
2022-10-25 11:41:01 +0530

    changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
-
-module.exports = {
-  darkMode: "class",
-  // ...
-};
+}
